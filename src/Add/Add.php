4b978ffc15c9ddb003e738dd39c6f1976fa5f9d7
brightwang <brightwang1984@gmail.com>
2018-06-05 17:55:30 +0800

    添加测试代码

---
+++ src/Add/Add.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brightwang
+ * Date: 2018/6/5
+ * Time: 下午5:50
+ */
+
+namespace Add;
+
+Class Add
+{
+    public function calc($a, $b)
+    {
+        return $a + $b;
+    }
+}
